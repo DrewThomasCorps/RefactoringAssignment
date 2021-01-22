e067f3f84b8c834ddae0b574c2de501e2a944000
Drew Thomas <amthomas12@bsu.edu>
2021-01-22 14:58:53 -0500

    Extracting methods

--- src/InsuranceCalculator.java
+++ src/InsuranceCalculator.java
@@ -1,15 +1,31 @@
 public class InsuranceCalculator {
 
     public double calculateInsurance(double income) {
-        if (income <= 10000) {
-            return income*0.365;
-        } else if (income <= 30000) {
-            return (income-10000)*0.2+35600;
+        if (income <= 10_000) {
+            return income * 0.365;
+        } else if (income <= 30_000) {
+            return (income - 10_000) * 0.2 + 35_600;
         } else if (income <= 60000) {
-            return (income-30000)*0.1+76500;
+            return (income - 30_000) * 0.1 + 76_500;
         } else {
-            return (income-60000)*0.02+105600;
+            return calculateInsuranceVeryHigh(income);
         }
+    }
+
+    private double calculateInsuranceVeryHigh(double income) {
+        return (income - getAdjustment()) * getWeight() + getConstant();
+    }
+
+    private int getConstant() {
+        return 105_600;
+    }
+
+    private double getWeight() {
+        return 0.02;
+    }
+
+    private int getAdjustment() {
+        return 60_000;
     }
 
 }
